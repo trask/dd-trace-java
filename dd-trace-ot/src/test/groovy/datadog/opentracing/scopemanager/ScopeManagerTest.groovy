075c6d630239f2ca0313f8eef4dd25f5a5bcb747
Tyler Benson <tyler.benson@datadoghq.com>
2019-01-02 16:25:32 -0800

    Changes required for OT 0.32
    This seems backwards compatable with 0.31 since all changes are additional, so this should be safe.

--- dd-trace-ot/src/test/groovy/datadog/opentracing/scopemanager/ScopeManagerTest.groovy
+++ dd-trace-ot/src/test/groovy/datadog/opentracing/scopemanager/ScopeManagerTest.groovy
@@ -600,8 +600,19 @@ class ScopeManagerTest extends Specification {
     }
 
     @Override
+    Scope activate(Span span) {
+      set(span)
+      return this
+    }
+
+    @Override
     Scope active() {
       return get() == null ? null : this
+    }
+
+    @Override
+    Span activeSpan() {
+      return active()?.span()
     }
 
     String toString() {
